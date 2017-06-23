59e7a1733b1ad1e80f179ad3389fd73901d554a5
Apurv Bhavsar <bapurv557@gmail.com>
2017-06-23 11:25:40 -0400

    Update Country.java

--- src/main/java/com/apurv/entity/Country.java
+++ src/main/java/com/apurv/entity/Country.java
@@ -1,12 +1,10 @@
 package com.apurv.entity;
 
 import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
