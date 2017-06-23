1cc99409fc985f5719ec325d22d041ae34dea153
Apurv Bhavsar <bapurv557@gmail.com>
2017-06-23 11:22:21 -0400

    Update Average.java

--- src/main/java/com/apurv/entity/Average.java
+++ src/main/java/com/apurv/entity/Average.java
@@ -1,19 +1,20 @@
 package com.apurv.entity;
 
 public class Average {
-private String title;
-private String average;
-public String getTitle() {
+  private String title;
+  private String average;
+  public String getTitle() {
 	return title;
-}
-public void setTitle(String title) {
+     	}
+	
+  public void setTitle(String title) {
 	this.title = title;
-}
-public String getAverage() {
+     	}
+	
+  public String getAverage() {
 	return average;
-}
-public void setAverage(String average) {
+	}
+  public void setAverage(String average) {
 	this.average = average;
-}
-   
-}
+	}
+   }
