b3f38ac14e39f6e4839e4ab03cea9d39ba3ff6da
Apurv Bhavsar <bapurv557@gmail.com>
2017-06-23 11:35:52 -0400

    Update Rate.java

--- src/main/java/com/apurv/entity/Rate.java
+++ src/main/java/com/apurv/entity/Rate.java
@@ -1,24 +1,21 @@
 package com.apurv.entity;
-import java.io.Serializable;
 
+import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Table
-
 public class Rate  implements Serializable {
 	@Id
 	@GenericGenerator(name="customUUID", strategy="uuid2")
 	@GeneratedValue(generator="customUUID")
 	private String rateId;
-	
 	private String userName;
 	private String title;
 	private String rate;
@@ -27,36 +24,34 @@ public class Rate  implements Serializable {
 	
 	public String getRateId() {
 		return rateId;
-	}
+		}
 
 	public void setRateId(String rateId) {
 		this.rateId = rateId;
-	}
+		}
 
 	public String getTitle() {
 		return title;
-	}
+		}
 
 	public void setTitle(String title) {
 		this.title = title;
-	}
+		}
 
 	public String getRate() {
 		return rate;
-	}
+		}
 
 	public void setRate(String rate) {
 		this.rate = rate;
-	}
+		}
 
 	
 	public String getUserName() {
 		return userName;
-	}
+		}
+	
 	public void setUserName(String userName) {
 		this.userName = userName;
-	}
-	
-
-	 
+		}
 }
