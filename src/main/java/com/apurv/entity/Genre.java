14d5cea35e7c2fc349251eb70a779457465941f5
Apurv Bhavsar <bapurv557@gmail.com>
2017-06-23 11:28:01 -0400

    Update Genre.java

--- src/main/java/com/apurv/entity/Genre.java
+++ src/main/java/com/apurv/entity/Genre.java
@@ -1,12 +1,10 @@
 package com.apurv.entity;
 
 import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
@@ -16,25 +14,27 @@ public class Genre  implements Serializable {
 	@GenericGenerator(name="customUUID", strategy="uuid2")
 	@GeneratedValue(generator="customUUID")
 	private String genreId;
-	
 	private String genre;
+	
 	public Genre() {}
 
-	  public Genre(String name) {
+	public Genre(String name) {
 	    this.genre = name;
-	  }
+	    }
+	
 	public String getGenreId() {
 		return genreId;
-	}
+		}
+	
 	public void setGenreId(String genreId) {
 		this.genreId = genreId;
-	}
+		}
 	
 	public String getGenre() {
 		return genre;
 	}
+	
 	public void setGenre(String genre) {
 		this.genre = genre;
-	}
-	
+	}	
 }
