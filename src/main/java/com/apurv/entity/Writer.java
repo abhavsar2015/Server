a38f8a88683d0dae1a8119eeafd820e324bca3ff
Apurv Bhavsar <bapurv557@gmail.com>
2017-06-23 11:37:01 -0400

    Update Writer.java

--- src/main/java/com/apurv/entity/Writer.java
+++ src/main/java/com/apurv/entity/Writer.java
@@ -1,12 +1,10 @@
 package com.apurv.entity;
 
 import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
@@ -16,25 +14,27 @@ public class Writer implements Serializable {
 	@GenericGenerator(name="customUUID", strategy="uuid2")
 	@GeneratedValue(generator="customUUID")
 	private String wriId;
-    
-    private String writer;
-    public Writer() {}
+    	private String writer;
+    	
+	public Writer() {}
 
-	  public Writer(String name) {
-	    this.writer = name;
-	  }
+	public Writer(String name) {
+	    	this.writer = name;
+	  	}
+	
 	public String getWriId() {
 		return wriId;
-	}
+		}
+	
 	public void setWriId(String wriId) {
 		this.wriId = wriId;
-	}
+		}
 	
 	public String getWriter() {
 		return writer;
-	}
+		}
+	
 	public void setWriter(String writer) {
 		this.writer = writer;
-	}
-	
+		}
 }
