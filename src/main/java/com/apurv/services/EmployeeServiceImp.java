ac9b7a94c419830913cdd0b0bcb063bdce7c1e79
Apurv Bhavsar <bapurv557@gmail.com>
2017-06-23 11:48:42 -0400

    Update EmployeeServiceImp.java

--- src/main/java/com/apurv/services/EmployeeServiceImp.java
+++ src/main/java/com/apurv/services/EmployeeServiceImp.java
@@ -4,14 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
-
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import com.apurv.entity.Average;
 import com.apurv.entity.Comment;
 import com.apurv.entity.Login;
@@ -22,54 +19,45 @@ import com.apurv.repository.EmployeeRepository;
 
 @Service("empSer")
 public class EmployeeServiceImp implements EmployeeService {
-   Movie emp;
-   
-@Autowired
-EmployeeRepository repository;
+   	Movie emp;
+	
+	@Autowired
+	EmployeeRepository repository;
+	
 	@Override
 	public List<Movie> empAll() {
-       
 		return repository.empAll();
-	}
+		}
+	
 	@Override
 	public List<Login> getLogin(String id) {
-		// TODO Auto-generated method stub
-		
-	    return repository.getLogin(id); 
-		//Login login= em.find(Login.class, userNam
-		//return login;
-	}
+	    	return repository.getLogin(id); 
+	    	}
+	
 	@Override
 	public List<Comment> getAllComments(String title) {
-		// TODO Auto-generated method stub
-		
-	    return repository.getAllComments(title); 
-		//Login login= em.find(Login.class, userNam
-		//return login;
-	}
+	    	return repository.getAllComments(title); 
+		}
+	
 	@Override
 	public String deleteMovie(String movieId) {
-		// TODO Auto-generated method stub
-		
-	    return repository.deleteMovie(movieId); 
-		
-	}
+	    	return repository.deleteMovie(movieId); 
+		}
 	
 	@Override
 	public Movie getOne(String id) {
 		// TODO Auto-generated method stub
 		return repository.getOne(id);
-	}
+		}
 
 	@Override
 	public List<Average> getAverage(List<String> titles) {
 		// TODO Auto-generated method stub
 		return repository.getAverage(titles);
-	}
+		}
 	
 	@Override
-	public String addRate(Rate rate)  
-	{
+	public String addRate(Rate rate)  {
 		try {
 			return repository.addRate(rate);
 		} catch (Exception e) {
@@ -77,10 +65,10 @@ EmployeeRepository repository;
 			e.printStackTrace();
 		}
 		return null;
-	}
+		}
+	
 	@Override   
-	public String addComment(Comment comment) 
-	{
+	public String addComment(Comment comment) {
 		try {
 			return repository.addComment(comment);
 		} catch (Exception e) {
@@ -88,7 +76,8 @@ EmployeeRepository repository;
 			e.printStackTrace();
 		}
 		return null;
-	}
+		}
+	
 	@Override
 	public String addLogin(final Login login) {
 		// TODO Auto-generated method stub
@@ -99,15 +88,15 @@ EmployeeRepository repository;
 			e.printStackTrace();
 		}
 		return null;
-	}
+		}
+	
 	@Override
-	public String updateMovie(String movieId,Movie movie) throws Exception
-	{
+	public String updateMovie(String movieId,Movie movie) throws Exception{
 		return repository.updateMovie(movieId,movie);
-	}
+		}
+	
 	@Override
 	public String addOne(final Movie movie)  {
-		
 		try {
 		String h=repository.addOne(movie);
 			return h;
@@ -116,6 +105,5 @@ EmployeeRepository repository;
 			e.printStackTrace();
 		}
 		return null;
-	}
-	
+		}
 }
