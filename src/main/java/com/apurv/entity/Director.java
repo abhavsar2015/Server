1a74d5135eca3ff0fa2315c18476e6678eb8c00f
Apurv Bhavsar <bapurv557@gmail.com>
2017-06-23 11:26:51 -0400

    Update Director.java

--- src/main/java/com/apurv/entity/Director.java
+++ src/main/java/com/apurv/entity/Director.java
@@ -1,12 +1,10 @@
 package com.apurv.entity;
 
 import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
@@ -15,26 +13,29 @@ public class Director implements Serializable {
 	@Id
 	@GenericGenerator(name="customUUID", strategy="uuid2")
 	@GeneratedValue(generator="customUUID")
-    private String dirId;
+    	private String dirId;
+    	private String director;
     
-    private String director;
-    public Director() {}
+	public Director() {}
 
-    public Director(String name) {
-      this.director = name;
-    }
+    	public Director(String name) {
+      		this.director = name;
+    		}
+	
 	public String getDirId() {
 		return dirId;
-	}
+		}
+	
 	public void setDirId(String dirId) {
 		this.dirId = dirId;
-	}
+		}
 	
 	public String getDirector() {
 		return director;
-	}
+		}
+	
 	public void setDirector(String director) {
 		this.director = director;
-	}
+		}
     
 }
