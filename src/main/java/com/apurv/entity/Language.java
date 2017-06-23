93f00fdf3acbbb6750d9a56456dff475e19dd48b
Apurv Bhavsar <bapurv557@gmail.com>
2017-06-23 11:29:23 -0400

    Update Language.java

--- src/main/java/com/apurv/entity/Language.java
+++ src/main/java/com/apurv/entity/Language.java
@@ -1,12 +1,10 @@
 package com.apurv.entity;
 
 import java.io.Serializable;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
@@ -16,24 +14,27 @@ public class Language implements Serializable {
 	@GenericGenerator(name="customUUID", strategy="uuid2")
 	@GeneratedValue(generator="customUUID")
 	private String langId;
+	private String language;
+	
 	public Language() {}
 
-	  public Language(String name) {
+	public Language(String name) {
 	    this.language = name;
-	  }
-	private String language;
+	    }
+	
 	public String getLangId() {
 		return langId;
-	}
+		}
+	
 	public void setLangId(String langId) {
 		this.langId = langId;
-	}
+		}
 	
 	public String getLanguage() {
 		return language;
-	}
+		}
+	
 	public void setLanguage(String language) {
 		this.language = language;
 	}
-	
 }
